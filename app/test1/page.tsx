7fc7982c6376b16571752870c731537bd5d0dfa6
CosarcaLiviu <cosarca.liviu@gmail.com>
2026-06-12 18:40:47 +0300

    acordion

--- app/test1/page.tsx
+++ app/test1/page.tsx
@@ -1,17 +1,10 @@
-import CardCar from "@/components/pages/home/card-car";
-// import Something from "@/components/common/something";
-// import PopularMakes from "@/components/common/popular-makes"
-// import CardCarRight from "@/components/pages/home/card-car-right";
-// import SellBuy from "@/components/common/sell-buy";
-// import Footer from "@/components/common/footer-section"
-import Scroll from "@/components/pages/home/upp-button";
+import Acordion from "@/components/practice/acordion";
+
 function Test1() {
-  // return <CardCar />;
   return (
-    <>
-      <Scroll />
-      {/* <CardCar car={car} /> */}
-    </>
+    <div>
+      <Acordion />
+    </div>
   );
 }
 
